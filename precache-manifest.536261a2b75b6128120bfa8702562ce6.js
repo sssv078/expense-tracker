9d7d9bbd88b785458f631e7929b7e2eb7a01312f
sssv078 <surajsri078@gmail.com>
2021-11-10 19:15:18 +0530

    Updates

---
+++ precache-manifest.536261a2b75b6128120bfa8702562ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be051f6cb810aa946d1d71919ae5512",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "215d7abe4f7a11ee9e09",
+    "url": "/expense-tracker/static/css/main.06e2107b.chunk.css"
+  },
+  {
+    "revision": "9e39265413187adef2ab",
+    "url": "/expense-tracker/static/js/2.dbf36497.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/expense-tracker/static/js/2.dbf36497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "215d7abe4f7a11ee9e09",
+    "url": "/expense-tracker/static/js/main.5e0b6440.chunk.js"
+  },
+  {
+    "revision": "dbcccea09f21a885e02a",
+    "url": "/expense-tracker/static/js/runtime-main.65372eef.js"
+  }
+]);
